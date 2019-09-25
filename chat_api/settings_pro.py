c45e166d139172cd090788bb082d0a407e3249cc
Zuxriddin Kamalov <bigjoss@mail.ru>
2019-09-25 11:17:22 +0500

    Fix file upload

--- chat_api/settings_pro.py
+++ chat_api/settings_pro.py
@@ -57,8 +57,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'GUI.apps.GuiConfig',
-    'GUI',
+    'GUI.apps.GuiConfig',
+    # 'GUI',
     'corsheaders',
     'django_extensions',
 ]
